b43e8a32ab4996c70bf50421984e137d8805e030
gfw-breaker <lixiaoming0513@gmail.com>
2021-07-15 02:12:08 +0000

    ok

--- set-fw.sh
+++ set-fw.sh
@@ -2,13 +2,16 @@
 
 url="https://www.cloudflare.com/ips-v4"
 
-wget $url -O cidrs.txt
+# Remove service
+firewall-cmd --permanent --remove-service=ssh
 
+wget $url -O cf.txt
+# Allow CloudFlare
 firewall-cmd --permanent --new-ipset=cf --type=hash:net
-firewall-cmd --permanent --ipset=cf --add-entries-from-file=cidrs.txt
+firewall-cmd --permanent --ipset=cf --add-entries-from-file=cf.txt
 
-
-
-#firewall-cmd --permanent --new-ipset=mgmt --type=hash:net
+# Allow MGMT 
+firewall-cmd --permanent --new-ipset=mgmt --type=hash:net
+firewall-cmd --permanent --ipset=mgmt --add-entries-from-file=mgmt.txt
 
 
